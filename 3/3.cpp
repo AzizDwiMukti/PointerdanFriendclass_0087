fc9fea1304b304fc951f2528d978ed21c2c8c7c1
ASUS <ASUS@AZIZDWIMUKTI>
2024-05-06 15:20:25 +0700

    input void

--- 3/3.cpp
+++ 3/3.cpp
@@ -2,11 +2,37 @@
 using namespace std;
 
 class siswa;
-class orang;
+class orang
+
 {
 private:
 	string nama;
 
-	public void setNama(string pNama);
+public:
+	void setNama(string pNama);
 	friend class siswa;
 };
+
+class siswa
+{
+private:
+	int id;
+public:
+	void setId(int pId);
+	void displayAll(orang& a);
+
+};
+
+void siswa::displayAll(orang& a)
+{
+	cout << id << endl
+		<< a.nama;
+}
+void orang::setNama(string pNama)
+{
+	nama = pNama;
+}
+void siswa::setId(int pId)
+{
+	id = pId;
+}
